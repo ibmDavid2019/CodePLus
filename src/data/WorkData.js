7c61fbcffd3342fb9bbf26ac4d62ec4de8de39d2
david <davidlukas145@gmail.com>
2023-06-10 13:52:07 -0300

    atualização

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -3,21 +3,42 @@
 export const Work = [
     {
         id:1,
-        name:"Kali Linux",
-        description:"distribuição para Pentest e Cibersegurança",
-        tags:["Linux","Pentest","Cibersegurança"],
+        name:"Agencia Landing Page",
+        description:"este é um blog criado em ReaactJS.",
+        tags:["react","gsap","styledComponents"],
         
         demo:"https://agency-website-eta.vercel.app/",
         github:"https://github.com/codebucks27/Agency-website"
     },
     {
         id:2,
-        name:"React",
-        description:"Linguagem de programação para desenvolver Websites e aplicativos.",
+        name:"Bootstrap",
+        description:"Conhecemos tecnologias como bootstrap",
         tags:["react","sass"],
         
         demo:"http://react-job-listing-website.codebucks27.vercel.app/",
         github:"https://github.com/codebucks27/react-job-listing-website"
         
-    }  
+    },
+    {
+        id:3,
+        name:"Site Clonado",
+        description:"Site Clonado da CodeBucks.",
+        tags:["react","api","sass"],
+        
+        demo:"http://devto-clone-codebucks27.vercel.app/",
+        github:"https://github.com/codebucks27/devto-clone"
+    },
+    {
+        id:6,
+        name:"Responsive Navigation Menu",
+        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+        tags:["react","react-router","css"],
+        
+        demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
+        github:"https://github.com/codebucks27/React-responsive-navbar"
+        
+    },
+    
+  
 ] 
